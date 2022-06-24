7723f10e384c6ebd2325f715b10768f5af124a3e
nicolasnasr <nasr528@gmail.com>
2022-06-24 12:41:14 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace E_Learning_I3332.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
